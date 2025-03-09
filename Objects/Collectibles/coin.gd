e5cc84c1bb9eb9844d77884bac80941f9744e978
Edhddz <103544174+Edhddz@users.noreply.github.com>
2025-03-09 10:11:43 -0600

    Lesson #19: Coins heads-up display
    + Added HUD to keep track of collected coins
    + Added function to update coin values on HUD
    + Enabled TAA by default

--- Objects/Collectibles/coin.gd
+++ Objects/Collectibles/coin.gd
@@ -1,6 +1,7 @@
 extends Area3D
 
 const ROT_SPEED: int = 2  # Number of degrees the coin rotates every frame
+@export var hud: CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,17 +13,20 @@ func _process(_delta: float) -> void:
 	rotate_y(deg_to_rad(ROT_SPEED))
 
 func _on_body_entered(_body: Node3D) -> void:  # Built-in signal
-	Global.coins += 1
-	print(Global.coins)
 	disable_coin_collision()
+	update_coins()
 	$AnimationPlayer.play("bounce")
 	
+
+func update_coins():
+	Global.coins += 1
+	hud.get_node("CoinsLabel").text = str(Global.coins)
 
 func disable_coin_collision():
 	set_collision_layer_value(3, false)
 	set_collision_mask_value(1, false)
 
-func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
 	queue_free()
 	
 	if Global.coins >= Global.NUM_COINS_TO_WIN:
--- scenes/level_1.gd
+++ scenes/level_1.gd
@@ -3,3 +3,4 @@ extends Node3D
 
 func _ready() -> void:
 	Global.coins = 0 # Reset number of collected coins on level load
+	$HUD.get_node("CoinsLabel").text = str(Global.coins)
